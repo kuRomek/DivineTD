3ede007a7d53d4b143d15a5f3e442c5fd49dd894
kuRomek <qustes@gmail.com>
2026-06-01 18:02:47 +0400

    feat: create animations in main menu

--- Assets/Source/UI/MainMenu.cs
+++ Assets/Source/UI/MainMenu.cs
@@ -2,27 +2,65 @@ using NaughtyAttributes;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class MainMenu : MonoBehaviour
 {
+    private const float ShiftAnimationDuration = 0.2f;
+
     [SerializeField] private Button _playButton;
     [SerializeField] private Button _switchFactionButton;
+    [SerializeField] private RectTransform _switchButtonSprite;
+    [SerializeField] private RectTransform _heavenScreen;
+    [SerializeField] private RectTransform _hellScreen;
     [Scene, SerializeField] private string _gameplayScene;
+
+    private Tween _screenShifting;
+    private Tween _switchButtonRotation;
 
     private void Start()
     {
+        _heavenScreen.anchoredPosition = default;
+        _hellScreen.anchoredPosition = new Vector2(0, -((RectTransform)transform).rect.height);
+        GameState.SwitchFaction(true);
+
         _playButton.onClick.AddListener(LoadGameplayScene);
         _switchFactionButton.onClick.AddListener(SwitchFaction);
-        SwitchFaction();
     }
 
     private void SwitchFaction()
     {
         GameState.SwitchFaction(GameState.IsCurrentFactionHeaven == false);
+        ShiftFactionScreen(true);
     }
 
     private void LoadGameplayScene()
     {
         SceneManager.LoadScene(_gameplayScene);
+    }
+
+    private void ShiftFactionScreen(bool withAnimation)
+    {
+        float height = ((RectTransform)transform).rect.height;
+        bool toHeaven = GameState.IsCurrentFactionHeaven;
+
+        _screenShifting?.Kill();
+        _switchButtonRotation?.Complete();
+
+        if (withAnimation)
+        {
+            _screenShifting = DOTween.Sequence().
+                Join(_heavenScreen.DOAnchorPos(new Vector2(0f, toHeaven ? 0f : height), ShiftAnimationDuration)).
+                Join(_hellScreen.DOAnchorPos(new Vector2(0f, toHeaven ? -height : 0f), ShiftAnimationDuration));
+
+            _switchButtonRotation = _switchButtonSprite.DORotate(Vector3.forward * 180f, ShiftAnimationDuration).
+                SetEase(Ease.OutBack).SetRelative();
+        }
+        else
+        {
+            _heavenScreen.anchoredPosition = new Vector2(0f, toHeaven ? 0f : height);
+            _hellScreen.anchoredPosition = new Vector2(0f, toHeaven ? -height : 0f);
+            _switchButtonSprite.Rotate(Vector3.forward * 180f);
+        }
     }
 }
